7ce287003153d345826c86b6b44d8505628018c8
Sherlockey <118456803+Sherlockey@users.noreply.github.com>
2025-02-22 22:17:46 -0500

    final

--- scripts/global.gd
+++ scripts/global.gd
@@ -8,24 +8,26 @@ signal enemy_undied(node: Node)
 signal exit_area_entered()
 
 var is_rewinding: bool = false
-var audio_bus_layout: AudioBusLayout = preload("res://resources/new_audio_bus_layout.tres")
-
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	process_mode = PROCESS_MODE_ALWAYS
-	AudioServer.set_bus_layout(audio_bus_layout)
 
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("rewind"):
 		is_rewinding = true
 		get_tree().paused = false
+	else:
+		is_rewinding = false
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("rewind"):
 		AudioServer.set_bus_effect_enabled(0, 0, true)
 		AudioServer.set_bus_effect_enabled(0, 1, true)
 		AudioServer.set_bus_effect_enabled(0, 2, true)
-	else:
-		is_rewinding = false
+	if Input.is_action_just_released("rewind"):
 		AudioServer.set_bus_effect_enabled(0, 0, false)
 		AudioServer.set_bus_effect_enabled(0, 1, false)
 		AudioServer.set_bus_effect_enabled(0, 2, false)
